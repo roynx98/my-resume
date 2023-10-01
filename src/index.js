7f76778352fce4da550e2605b0fc158d64a7bfb6
roynx98 <roynx98@gmail.com>
2023-10-01 13:53:20 -0600

    feat: Update React.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Resume } from './Resume';
 
-ReactDOM.render(
-  <Resume />,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <Resume />
 );
+
+// ReactDOM.render(
+//   <Resume />,
+//   document.getElementById('root')
+// );
