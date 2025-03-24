0d685eca0f3c96bb281a013984da2e6c2fb4aeea
roynx98 <roynx98@gmail.com>
2025-03-24 12:34:12 -0600

    feat(Skill): Change layout to one row.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 My resume made with react, [see the live version](https://roynx98.github.io/my-resume/)
 
 In order to save it as PDF use the following config:
-* Paper size: A3
+* Paper size: A4
 * Margins: None
-* Scale: 125
+* Scale: 95
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,58 +2,30 @@ import React from 'react';
 import './Skills.css'
 
 export const Skills = () => {
-  const sizes = [
-    120, 250, 100, 80
+  const skills = [
+    { name: 'Javascript', img: 'imgs/js.svg' },
+    { name: 'Typescript', img: 'imgs/typescript.png' },
+    { name: 'Node', img: 'imgs/next-js.svg' },
+    { name: 'React', img: 'imgs/react.svg' },
+    { name: 'Git', img: 'imgs/git.svg' },
+    { name: 'AWS', img: 'imgs/aws.png' },
+    { name: 'Docker', img: 'imgs/docker.png' },
   ];
-  const data = [
-    {
-      title: 'Languages',
-      items: [
-        { name: 'Javascript', img: 'imgs/js.svg' },
-        { name: 'Typescript', img: 'imgs/swift.svg' }
-      ]
-    },
-    {
-      title: 'Libraries/Frameworks',
-      items: [
-        { name: 'Node', img: 'imgs/next-js.svg' },
-        { name: 'React', img: 'imgs/react.svg' },
-      ]
-    },
-    {
-      title: 'Tools',
-      items: [
-        { name: 'Git', img: 'imgs/git.svg' },
-        { name: 'AWS', img: 'imgs/aws.png' },
-        { name: 'Docker', img: 'imgs/docker.png' },
-        { name: 'PostgreSQL', img: 'imgs/postgres.png' },
-      ]
-    },
 
-  ];
   return (
     <div>
       <h2 className="skills-title">Skills</h2>
+
       <div className="skills-container">
         {
-          data.map((skill, i) =>
-            <div key={i} style={{ flexGrow: 1 }}>
-              <h3>{skill.title}</h3>
-              <div className="skills-section" style={{ width: sizes[i] }}>
-                {
-                  skill.items.map((item, j) =>
-                    <div key={j} className="skills-item">
-                      <img src={item.img}></img>
-                      <p>{item.name}</p>
-                    </div>
-                  )
-                }
-              </div>
+          skills.map(({ name, img }) =>
+            <div key={name} className="skills-item">
+              <img src={img} alt=''></img>
+              <p>{name}</p>
             </div>
           )
         }
       </div>
-
     </div>
   );
 };
